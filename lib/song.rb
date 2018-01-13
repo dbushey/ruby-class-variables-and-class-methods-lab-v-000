cef42185d300d12c0f0238136db9021703408be7
Daiane Bushey <daianebushey@gmail.com>
2018-01-13 22:47:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  @@song_count = 0
+  @@count = 0
   @@genres = []
   @@artists = []
 
@@ -11,7 +11,7 @@ class Song
     @genre = genre
     @@genres << genre
     @@artists << artist
-    @@song_count += 1
+    @@count += 1
   end
 
   def self.count
